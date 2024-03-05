62c87eb4c7507152f6df7a0b485356efe0c1506d
Honzoraptor <j.ackermann@email.cz>
2024-03-05 10:12:39 +0100

    Inserting now fully works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,48 +6,55 @@
     PUBLIC_APPWRITE_COLLECTION,
   } from "$env/static/public";
   let url = "";
-  let existsInDB = false;
 
   const databases = new Databases(client);
 
-  function shorten() {
-    try {
-      let getDataPromise = databases.listDocuments(
-        PUBLIC_APPWRITE_DB,
-        PUBLIC_APPWRITE_COLLECTION,
-        [Query.equal("hash", "test")],
-      );
-      getDataPromise.then(
-        function (response) {
-          if (response.total > 0) {
-            // the hash already exists in the db and I have generate a new one.
-            console.log("Hash already exists");
-          } else {
-            console.log("hash dosn't exist");
-            const insertPromise = databases.createDocument(
-              PUBLIC_APPWRITE_DB,
-              PUBLIC_APPWRITE_COLLECTION,
-              ID.unique(),
-              { url: url, hash: "test" },
-            );
-
-            insertPromise.then(
-              function (response) {
-                console.log(response);
-              },
-              function (error) {
-                console.log(error);
-              },
-            );
-          }
-        },
-        function (error) {
-          console.log(error);
-        },
-      );
-    } catch (e) {
-      console.error(`Error in the try/catch: ${e}`);
+  function hash() {
+    let chars =
+      "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzY1234567890";
+    let result = "";
+    for (let i = 0; i < 8; i++) {
+      result += chars[Math.floor(Math.random() * chars.length)];
     }
+    return result;
+  }
+
+  function shorten() {
+    let randomHash = hash();
+    let getDataPromise = databases.listDocuments(
+      PUBLIC_APPWRITE_DB,
+      PUBLIC_APPWRITE_COLLECTION,
+      [Query.equal("hash", randomHash)],
+    );
+    getDataPromise.then(
+      function (response) {
+        if (response.total > 0) {
+          // the hash already exists in the db and I have generate a new one.
+          console.log("Hash already exists");
+          shorten();
+        } else {
+          console.log("hash dosn't exist");
+          const insertPromise = databases.createDocument(
+            PUBLIC_APPWRITE_DB,
+            PUBLIC_APPWRITE_COLLECTION,
+            ID.unique(),
+            { url: url, hash: randomHash },
+          );
+
+          insertPromise.then(
+            function (response) {
+              console.log(response);
+            },
+            function (error) {
+              console.log(error);
+            },
+          );
+        }
+      },
+      function (error) {
+        console.log(error);
+      },
+    );
   }
 </script>
 
