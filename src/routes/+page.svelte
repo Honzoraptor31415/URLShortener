509b56b7cfc9ba324215c61939bf51830e32b1d6
Honzoraptor <j.ackermann@email.cz>
2024-03-09 16:07:57 +0100

    General polishing
    Mainly url validation with ts instead of using the input type=url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { Databases, ID, Query } from "appwrite";
   import { client } from "$lib/appwrite";
   import {
@@ -6,10 +6,10 @@
     PUBLIC_APPWRITE_COLLECTION,
   } from "$env/static/public";
   import CopyIcon from "$lib/icons/CopyIcon.svelte";
-  import { browser } from "$app/environment";
   let url = "";
   let urlHash = "";
   let showCopiedMsg = false;
+  let urlLabel = "";
 
   const databases = new Databases(client);
 
@@ -36,43 +36,62 @@
     }, 1390);
   }
 
-  function shorten() {
-    let randomHash = hash();
-    let getDataPromise = databases.listDocuments(
-      PUBLIC_APPWRITE_DB,
-      PUBLIC_APPWRITE_COLLECTION,
-      [Query.equal("hash", randomHash)],
-    );
-    getDataPromise.then(
-      function (response) {
-        if (response.total > 0) {
-          // the hash already exists in the db and I have generate a new one.
-          console.log("Hash already exists");
-          shorten();
-        } else {
-          console.log("hash dosn't exist");
-          const insertPromise = databases.createDocument(
-            PUBLIC_APPWRITE_DB,
-            PUBLIC_APPWRITE_COLLECTION,
-            ID.unique(),
-            { url: url, hash: randomHash },
-          );
+  function urlCheck() {
+    if (url.length < 1) {
+      urlLabel = "URL can't be empty";
+      return false;
+    } else if (
+      !url.match(
+        /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/,
+      )
+    ) {
+      urlLabel = "Invalid URL";
+      return false;
+    } else {
+      urlLabel = "";
+      return true;
+    }
+  }
 
-          insertPromise.then(
-            function (response) {
-              console.log(response.hash);
-              urlHash = response.hash;
-            },
-            function (error) {
-              console.log(error);
-            },
-          );
-        }
-      },
-      function (error) {
-        console.log(error);
-      },
-    );
+  function shorten() {
+    if (urlCheck()) {
+      let randomHash = hash();
+      let getDataPromise = databases.listDocuments(
+        PUBLIC_APPWRITE_DB,
+        PUBLIC_APPWRITE_COLLECTION,
+        [Query.equal("hash", randomHash)],
+      );
+      getDataPromise.then(
+        function (response) {
+          if (response.total > 0) {
+            // the hash already exists in the db and I have generate a new one.
+            console.log("Hash already exists");
+            shorten();
+          } else {
+            console.log("hash dosn't exist");
+            const insertPromise = databases.createDocument(
+              PUBLIC_APPWRITE_DB,
+              PUBLIC_APPWRITE_COLLECTION,
+              ID.unique(),
+              { url: url, hash: randomHash },
+            );
+
+            insertPromise.then(
+              function (response) {
+                console.log(response.hash);
+                urlHash = response.hash;
+              },
+              function (error) {
+                console.log(error);
+              },
+            );
+          }
+        },
+        function (error) {
+          console.log(error);
+        },
+      );
+    }
   }
 </script>
 
@@ -87,14 +106,15 @@
       }}
     >
       <div class="form-element">
-        <label for="url">Your URL</label>
+        <label for="url" class={urlLabel !== "" ? "form-error" : ""}
+          >{urlLabel === "" ? "Your URL" : urlLabel}</label
+        >
         <input
           bind:value={url}
-          type="url"
+          type="text"
           id="url"
           class="rounded"
           placeholder="goes here!"
-          required
         />
       </div>
       {#if urlHash}
